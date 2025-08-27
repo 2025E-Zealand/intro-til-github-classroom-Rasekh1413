006d1f3c5f480e30b1161c54bbfdb9c75945ea1c
Rasekh <rasekh1413@icloud.com>
2025-08-27 12:53:41 +0200

    added informationv0.2

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,17 @@
+﻿
+public class Demo
+{
+    public string Navn { get; set; }
+    public int Alder { get; set; }
+
+    public Demo(string navn, int alder)
+    {
+        Navn = navn;
+        Alder = alder;
+    }
+    public override string ToString()
+    {
+        return $"{Navn} {Alder}";
+    }
+}
+ 
--- MyFirstGitHubClassRoomConsoleApp60/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp60/Program.cs
@@ -1,4 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//  Intro til GitHub Classroom fra opgave 1. linked https://github.com/2025E-Zealand/intro-til-github-classroom-Rasekh1413
 
+Console.WriteLine("Den er et eksampel");
+
+
+Bil ejebil = new Bil("BMW", 2009, "E60 - 5-Series");
+Demo Demo1 = new Demo("Rasekh", 20);
+Demo Demo2 = new Demo("Elaf", 20);
+
+
+Console.WriteLine($"{Demo1.Navn} er {Demo1.Alder} " + "år," + " og han har en bil, der hedder: " + $"{ejebil}");
+Console.WriteLine($"{Demo2.Navn} er {Demo2.Alder} " + "år," + " og hun har ikke en bil, som " +  $"{Demo1.Navn} " + "har, der hedder: " + $"{ejebil}");
 
