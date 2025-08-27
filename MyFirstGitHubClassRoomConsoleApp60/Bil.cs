d1b1cbba74d5f933d5d5e314b7692d5a4476b308
Rasekh <rasekh1413@icloud.com>
2025-08-27 12:53:41 +0200

    v0.2

---
+++ MyFirstGitHubClassRoomConsoleApp60/Bil.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+public class Bil
+{
+    
+    public string Bilnavn { get; set; }
+
+    public int Bilår { get; set; }
+    
+    public string Biltype { get; set; }
+
+
+    public Bil(string bilnavn, int bilår, string biltype)
+    {
+        Bilnavn = bilnavn;
+        Bilår = bilår;
+        Biltype = biltype;
+    }
+
+    public override string ToString()
+    {
+        return $"{Bilnavn} {Biltype} {Bilår}";
+    }
+
+}
+
--- MyFirstGitHubClassRoomConsoleApp60/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp60/Program.cs
@@ -1,4 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
+
+Console.WriteLine("Den er et eksampel");
+
+
+Bil ejebil = new Bil("BMW", 2009, "E60 - 5-Series");
+Demo Demo1 = new Demo("Rasekh", 20);
+Demo Demo2 = new Demo("Elaf", 20);
+
+
+Console.WriteLine($"{Demo1.Navn} er {Demo1.Alder} " + "år," + " og han har en bil, der hedder: " + $"{ejebil}");
+Console.WriteLine($"{Demo2.Navn} er {Demo2.Alder} " + "år," + " og hun har ikke en bil, som " +  $"{Demo1.Navn} " + "har, der hedder: " + $"{ejebil}");
 
